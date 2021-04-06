ee238963d1d0670b75e3b3c59c887feb3e7f43d3
Guilherme Tavares <guilhermetavalves@gmail.com>
2021-04-06 18:37:55 -0300

    rename api file

--- src/pages/api/parts.js
+++ src/pages/api/parts.js
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
+  console.log("console", req);
   res.status(200).json({
     parts: [
       {
