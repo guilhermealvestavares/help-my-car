6e7784874366af5ec1f35b90ff9860fb700e50ac
Guilherme Tavares <guilhermetavalves@gmail.com>
2021-04-05 21:45:32 -0300

    added WhoWeAre structure

---
+++ src/sections/WhoWeAre/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Wrapper, Title, GridFlex, GridFlexItem } from "./styles";
+
+const WhoWeAre = () => {
+  return (
+    <>
+      <Wrapper>
+        <Title>Lorem ipsum dolor sit amet</Title>
+        <GridFlex>
+          <GridFlexItem>Lorem ipsum</GridFlexItem>
+          <GridFlexItem>Lorem ipsum</GridFlexItem>
+          <GridFlexItem>Lorem ipsum</GridFlexItem>
+        </GridFlex>
+      </Wrapper>
+    </>
+  );
+};
+
+export default WhoWeAre;
---
+++ src/sections/WhoWeAre/styles.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  width: 100%;
+  background-color: #f8f8f8;
+  padding: 32px 0;
+  margin: 68px 0;
+`;
+
+const Title = styled.h1`
+  font-size: 28px;
+  text-align: center;
+`;
+
+const GridFlex = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const GridFlexItem = styled.div`
+  width: 380px;
+  min-height: 400px;
+  background-color: #ffffff;
+  border-radius: 8px;
+  padding: 16px 32px;
+
+  &:not(:last-child) {
+    margin-right: 16px;
+  }
+`;
+
+export { Wrapper, GridFlex, GridFlexItem, Title };
